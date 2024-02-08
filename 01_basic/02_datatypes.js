ae4bb224b8f415d524b38cdb2a8c7afdde812abf
Richa Chaudhary <91756503+richaexe@users.noreply.github.com>
2024-02-08 09:24:53 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+
+//alert("Hello") we are using nodejs, not browser
+
+let name ="Codespace"
+let age = 18
+let isLoggedIn = false
+
+    /*   Premitive Daatatypes   */
+// number -> 2 to power 53
+// bigint
+// string = ""
+// boolen = true/false
+// null = standalone value
+// undefined = (declared var but given any value )
+// symbol = to find uniqueness
+
+//object
+console.log(typeof null); // object
+console.log(typeof undefined) // undefined
+
