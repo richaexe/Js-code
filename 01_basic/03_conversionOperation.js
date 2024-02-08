f29c4311504921fe20adeb65ba8d88e7940b637a
Richa Chaudhary <91756503+richaexe@users.noreply.github.com>
2024-02-08 09:55:06 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score= "33abc"
+console.log(typeof score)
+  //    Conversion
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" = 33
+// "33abc" = NaN
+// true = 1 ; false = 0
+
+let isLoggedIn = 1
+let boolenisLoggedIn = Boolean(isLoggedIn)
+console.log(boolenisLoggedIn)
+
+let num = 33
+let numstring = String(num)
+console.log(numstring)
+console.log(typeof numstring)
