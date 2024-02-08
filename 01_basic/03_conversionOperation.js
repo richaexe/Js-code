49f4c987ae50fa52d588c849892c3df6056225f2
Richa Chaudhary <91756503+richaexe@users.noreply.github.com>
2024-02-08 10:17:35 +0000

    conversion of operation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,6 +1,6 @@
 let score= "33abc"
 console.log(typeof score)
-  //    Conversion
+           //    Conversion
 let valueInNumber = Number(score)
 console.log(typeof valueInNumber)
 console.log(valueInNumber)
@@ -17,3 +17,31 @@ let num = 33
 let numstring = String(num)
 console.log(numstring)
 console.log(typeof numstring)
+
+
+//  *************Operations**************//
+
+let value = 3
+let negvalue = -value
+console.log(negvalue)
+
+// console.log(2+2)
+// console.log(4-2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " world"
+str3 = str1 +str2
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true); // 1
+console.log(+""); // 0  (not a good code)
